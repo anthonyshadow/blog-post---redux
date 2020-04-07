9b5202162ff9ee238e4705dae25c14b0ca7c1bc7
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2020-04-07 13:09:07 -0400

    refactoring reducers to work better with redux, created new reducer and better approach to memoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,31 +1,69 @@
-import _ from 'lodash';
+import _ from 'lodash'; 
+//would use if using memoized fetchuser
+
+
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  
+  await dispatch(fetchPosts());
 
+//lodash map and uniq
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  
+  userIds.forEach(id => dispatch(fetchUser(id)));
 
+}
 
 //fetch all posts
 
-export const fetchPosts = () => (dispatch, getState) => {
-    jsonPlaceholder.get("/posts")
-    .then((response) => {
-      dispatch({ type: 'FETCH_POSTS', payload: response.data})
-    });   
-  };
+// export const fetchPosts = () => (dispatch, getState) => {
+//     jsonPlaceholder.get("/posts")
+//     .then((response) => {
+//       dispatch({ type: 'FETCH_POSTS', payload: response.data})
+//     });   
+//   };
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+  
+  dispatch({ type: 'FETCH_POSTS', payload: response.data })
+}
 
 //fetch individual users (see id parameter)
 
-  export const fetchUser = (id) => (dispatch, getState) => {
-    _fetchUser(id, dispatch);
+  export const fetchUser = (id) => async (dispatch, getState) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    
+    dispatch({ type: 'FETCH_USER', payload: response.data})  
   };
 
+  // export const fetchUser = (id) => (dispatch, getState) => {
+  //   jsonPlaceholder.get(`/users/${id}`)
+  //   .then((response) => {
+  //     dispatch({ type: 'FETCH_USER', payload: response.data})
+  //   });   
+  // };
 
-  //helper function
 
-  const _fetchUser = _.memoize((id, dispatch) => {
-    jsonPlaceholder.get(`/users/${id}`)
-    .then((response) => {
-      dispatch({ type: 'FETCH_USER', payload: response.data})
-    });   
-  })
+
+
+//alternative option below for fetchuser to only make one xhr request, 
+//however only able to call user once
+
+  // //fetch individual users (see id parameter)
+
+  // export const fetchUser = (id) => (dispatch, getState) => {
+  //   _fetchUser(id, dispatch);
+  // };
+
+
+  // //helper function to only make 1 xhr call per user for fetchUser action
+
+  // const _fetchUser = _.memoize((id, dispatch) => {
+  //   jsonPlaceholder.get(`/users/${id}`)
+  //   .then((response) => {
+  //     dispatch({ type: 'FETCH_USER', payload: response.data})
+  //   });   
+  // })
