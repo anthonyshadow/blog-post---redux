8723563e43876da435b2d458935eb54195750554
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2020-04-07 10:32:19 -0400

    create helper function to render each blog post

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -8,14 +8,31 @@ class PostList extends React.Component {
     this.props.fetchPosts();
   };
 
+  renderList() {
+    return this.props.posts.map (post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="largw middle aligned icon user"/> 
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      )
+    })
+  }
+
   render() {
-    console.log(this.props.posts)
     return (
       
-      <div>Post List</div>
+      <div className="ui relaxed divided list">{this.renderList()}</div>
     );
   };
 };
+
+//assigning values of state to props
 
 const mapStateToProps = (state) => {
   return { posts: state.posts };
