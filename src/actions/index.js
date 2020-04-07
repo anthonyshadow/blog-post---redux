08917ca4b9c0a4b6b85fb643c7edd608ec27426b
Anthony Shadowitz <anthony.shadowitz@gmail.com>
2020-04-07 12:14:56 -0400

    refactored reducer with memoized from lodash to make only 1 xhr call for each user instead of amount of instances of posts

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+
+
+
 
 //fetch all posts
 
@@ -12,8 +17,15 @@ export const fetchPosts = () => (dispatch, getState) => {
 //fetch individual users (see id parameter)
 
   export const fetchUser = (id) => (dispatch, getState) => {
+    _fetchUser(id, dispatch);
+  };
+
+
+  //helper function
+
+  const _fetchUser = _.memoize((id, dispatch) => {
     jsonPlaceholder.get(`/users/${id}`)
     .then((response) => {
       dispatch({ type: 'FETCH_USER', payload: response.data})
     });   
-  };
+  })
